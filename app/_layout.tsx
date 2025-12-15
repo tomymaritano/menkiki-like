594126d127005230bb892bb0e7ef38267b0658e5
tomymaritano <tomymaritano@gmail.com>
2025-12-15 12:08:32 -0300

    fix: use Slot instead of Stack to avoid type error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 
@@ -6,7 +6,7 @@ export default function RootLayout() {
   return (
     <View style={{ flex: 1, backgroundColor: "#000" }}>
       <StatusBar style="light" />
-      <Stack screenOptions={{ headerShown: false }} />
+      <Slot />
     </View>
   );
 }
